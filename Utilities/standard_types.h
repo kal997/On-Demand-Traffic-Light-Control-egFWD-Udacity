0ee553f2cc4fa162599917116e866262277f8559
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-09-06 14:14:19 +0200

    creates standard types header file

---
+++ Utilities/standard_types.h
@@ -0,0 +1,31 @@
+/*
+ * standard_types.h
+ *
+ * Created: 9/6/2022 2:06:35 PM
+ *  Author: KHALED
+ */ 
+
+
+#ifndef STANDARD_TYPES_H_
+#define STANDARD_TYPES_H_
+
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short u16;
+typedef signed short s16;
+
+typedef unsigned long u32;
+typedef signed long s32;
+
+typedef unsigned long long u64;
+typedef signed long long s64;
+
+#define NULLPTR ((void*)0)
+
+
+
+
+
+#endif /* STANDARD_TYPES_H_ */
