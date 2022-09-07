bfa4a7d13136120fc0901e0fcf20a5f2ba0c6ce4
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-09-07 12:33:58 +0200

    adds external interrupts registers macros

--- Utilities/registers.h
+++ Utilities/registers.h
@@ -10,7 +10,7 @@
 #define REGISTERS_H_
 
 /************************************************************************/
-/*                              DIO_Registers                           */
+/*                              DIO Registers                           */
 /************************************************************************/
 
 
@@ -35,7 +35,51 @@
 #define PIND (*(volatile unsigned char*)0x30)
 
 
+/************************************************************************/
+/*                      EXTERNAL INTERRUPTS Registers                   */
+/************************************************************************/
+/*
+ * Interrupt 0 Sense Control
+ISC01 ISC00 Description
+  0     0   The low level of INT0 generates an interrupt request.
+  0     1   Any logical change on INT0 generates an interrupt request.
+  1     0   The falling edge of INT0 generates an interrupt request.
+  1     1   The rising edge of INT0 generates an interrupt request.
+ * 
+ */
 
+/*
+ * Interrupt 1 Sense Control
+ISC11 ISC10 Description
+  0     0   The low level of INT1 generates an interrupt request.
+  0     1   Any logical change on INT1 generates an interrupt request.
+  1     0   The falling edge of INT1 generates an interrupt request.
+  1     1   The rising edge of INT1 generates an interrupt request.
+ *
+ */
+
+/* INT2_State
+	falling=0
+	rising=1
+*/
+#define MCUCSR (*(volatile unsigned char*)0x54)
+#define ISC2 6
+
+#define MCUCR (*(volatile unsigned char*)0x55)
+#define ISC00 0
+#define ISC01 1
+#define ISC10 2
+#define ISC11 3
+
+#define GICR (*(volatile unsigned char*)0x5B)
+#define INT1 7
+#define INT0 6
+#define INT2 5
+
+#define GIFR  (*(volatile unsigned char*)0x5A)
+#define INTF1 7
+#define INTF0 6
+#define INTF2 5
 
 
 
